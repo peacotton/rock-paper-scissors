7ba7d42c225cdbb00c5fd6c15e1f79b18bcc8c13
Peacotton <b.watts1997@hotmail.com>
2023-03-08 13:44:22 -0500

    Add browser functionality and remove console logs

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-// console.log(Math.round(Math.random() * 2))
+let computerScore = 0;
+let playerScore = 0;
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(button => button.addEventListener('click', playGame));
+
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', resetGame);
+
+const scoreboard = document.querySelector("#scoreboard");
+scoreboard.style.cssText = 'border: solid black; background: pink';
+
+const score = document.createElement('p');
+score.textContent = (`Computer: ${computerScore} | Player: ${playerScore}`);
+
+const playerChoice = document.createElement('p');
+const computerChoice = document.createElement('p');
+const roundStatus = document.createElement('p');
+
+scoreboard.appendChild(score);
+scoreboard.appendChild(playerChoice);
+scoreboard.appendChild(computerChoice);
+scoreboard.appendChild(roundStatus);
 
 function getComputerChoice() {
     let computerGuess = Math.round(Math.random() * 2);
@@ -14,66 +35,69 @@ function getComputerChoice() {
     return computerGuess;  
 }
 
-function getPlayerChoice(){
-    playerGuess = prompt("Choose one: Rock, Paper, Scissors");
-    playerGuess = playerGuess.toLowerCase();
-    return playerGuess;
+function playGame(e, computerGuess) {
+    playerGuess = this.id;
+    playerChoice.textContent = (`You chose: ${playerGuess}`);
+    computerGuess = getComputerChoice();
+    computerChoice.textContent = (`Computer chose: ${computerGuess}`);
+    playRound(playerGuess, computerGuess);
 }
 
-function playRound(computerGuess, playerGuess){
-    let winner;
+function playRound(playerGuess, computerGuess){
     if (computerGuess == playerGuess){
-        winner = "draw";
+        score.textContent = (`Computer: ${computerScore} | Player: ${playerScore}`);
+        roundStatus.textContent = ("ROUND DRAW!");
     }
     if (computerGuess == "rock" && playerGuess == "scissors") {
-        winner = "computer";    
+        computerScore++;
+        score.textContent = (`Computer: ${computerScore} | Player: ${playerScore}`);  
+        roundStatus.textContent = ("ROUND LOSE!");  
     }
     if (computerGuess == "scissors" && playerGuess == "paper") {
-        winner = "computer";    
+        computerScore++; 
+        score.textContent = (`Computer: ${computerScore} | Player: ${playerScore}`);
+        roundStatus.textContent = ("ROUND LOSE!");  
     }  
     if (computerGuess == "paper" && playerGuess == "rock") {
-        winner = "computer";    
+        computerScore++;  
+        score.textContent = (`Computer: ${computerScore} | Player: ${playerScore}`);
+        roundStatus.textContent = ("ROUND LOSE!");  
     }
     if (computerGuess == "rock" && playerGuess == "paper") {
-        winner = "player";    
+        playerScore++; 
+        score.textContent = (`Computer: ${computerScore} | Player: ${playerScore}`);
+        roundStatus.textContent = ("ROUND WIN!");  
     }
     if (computerGuess == "paper" && playerGuess == "scissors") {
-        winner = "player";    
+        playerScore++;
+        score.textContent = (`Computer: ${computerScore} | Player: ${playerScore}`);
+        roundStatus.textContent = ("ROUND WIN!"); 
     }
     if (computerGuess == "scissors" && playerGuess == "rock") {
-        winner = "player";    
+        playerScore++;
+        score.textContent = (`Computer: ${computerScore} | Player: ${playerScore}`);
+        roundStatus.textContent = ("ROUND WIN!"); 
+    }   
+    if (computerScore == 3) {
+        computerChoice.textContent = ("");
+        playerChoice.textContent = ("");
+        roundStatus.textContent = ("YOU LOSE!!!");
+        buttons.forEach(button => button.removeEventListener('click', playGame));
     }
-    return winner;      
-}
-
-function game(){
-    let compScore = 0;
-    let playerScore = 0;
-    while (compScore < 3 && playerScore < 3){
-        winner = playRound(getComputerChoice(), getPlayerChoice());
-        if (winner == "computer"){
-            compScore++;
-            console.log("Computer wins this round!");
-        }
-        if (winner == "player"){
-            playerScore++
-            console.log("You win this round!");
-        }
-        if (winner == "draw"){
-            console.log("Draw!");
-        }        
-        console.log("Player Score: " + playerScore);
-        console.log("Computer Score: " + compScore);
-    }
-
     if (playerScore == 3) {
-        console.log("You won! :)");
-    }
-    if (compScore == 3) {
-        console.log("You lost! :(");
+        computerChoice.textContent = ("");
+        playerChoice.textContent = ("");
+        roundStatus.textContent = ("YOU WIN!!!");
+        buttons.forEach(button => button.removeEventListener('click', playGame));
     }
 }
 
-game();
-// console.log(getPlayerChoice());
-// console.log(getComputerChoice());
+function resetGame() {
+    computerScore = 0;
+    playerScore = 0;
+    score.textContent = (`Computer: ${computerScore} | Player: ${playerScore}`);
+    computerChoice.textContent = ("");
+    playerChoice.textContent = ("");
+    roundStatus.textContent = ("");
+    buttons.forEach(button => button.addEventListener('click', playGame)); 
+}
